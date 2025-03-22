57a1ee3a0bb6d6f5d04976f9eec4e9ee2ae6d77c
pavlo <iris398322@gmail.com>
2025-03-22 14:21:54 +0200

    cleanup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base : "/solidgate-fe-test-task",
     plugins: [react(), tsconfigPaths()],
     css: {
         modules: {
